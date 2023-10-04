f66a4f553856ecedf3ce65ec6f3a5dadddfedc09
dilla widya <dillawidya0302@gmail.com>
2023-10-05 01:37:31 +0700

    commit

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
     <a href="#" class="logo"><img src="images/logo.png" alt="Logo"></a>
     <ul class="nav-links">
-        <li><a href="index.php">Home</a></li>
+        <li><a href="index.html">Home</a></li>
         <li><a href="produk.php">Produk</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
